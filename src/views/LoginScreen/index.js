0170e8cc1098c2cdb69c24d91f9ab8436de90911
Oliver Coimbra <olivercoimbra@gmail.com>
2023-07-19 20:48:23 -0400

    Add login and home page

--- src/views/LoginScreen/index.js
+++ src/views/LoginScreen/index.js
@@ -1,7 +1,51 @@
 import React from 'react';
 
-import { Text } from 'react-native';
+import { View, Image, TextInput, Button, StyleSheet } from 'react-native';
 
-const LoginScreen = () => <Text>LoginScreen</Text>;
+const LoginScreen = () => {
+  return (
+    <View style={style.container}>
+      <View style={style.imageContainer}>
+      <Image source={{uri: 'https://reactjs.org/logo-og.png'}} style= {style.image}/>
+      </View>
+      <View style={style.inputsContainer}>
+        <TextInput placeholder="user" style={style.textInput}/>
+        <TextInput placeholder="password" style={style.textInput} secureTextEntry={true}/>
+      </View>
+      <Button style={style.button} title="Sign in"/>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  image : {
+    width : 70,
+    height : 50,
+    alignSelf: 'center',
+  },
+  imageContainer : {
+    marginTop : 10,
+    marginBottom : 10,
+  },
+  inputsContainer : {
+  
+  },
+  textInput : {
+    backgroundColor: '#FFFFFF',
+    padding: 6,
+    marginTop: 4,
+    marginBottom: 12,
+    fontSize: 16,
+  },
+  button : {
+    backgroundColor : '#1976D2',
+  },
+  container : {
+    flex: 1,
+    justifyContent: 'center',
+    marginLeft: 18,
+    marginRight: 18,
+  },
+});
 
 export default LoginScreen;
